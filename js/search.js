4c4cec328278280cb70913350cd7e9a0718206f8
serious-hats-only <120053464+serious-hats-only@users.noreply.github.com>
2024-08-24 18:57:54 -0700

    initial files

---
+++ js/search.js
@@ -0,0 +1,14 @@
+const baseURL = 'https://www.giphy.com/search/';
+
+async function genRandomGif() {
+    let response = await fetch('https://random-word-api.vercel.app/api?words=1')
+    let randomWord = await response.text()
+
+    // Removes quotes and brackets from the raw JSON response
+    const cleanedString = randomWord.replace(/\[\s*"/, '');
+    const finalString = cleanedString.replace(/"\s*]$/, '');
+
+    const url = baseURL + finalString;
+  
+    open(url);
+}
